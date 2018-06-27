ca9315a4ff3e4e82e6c2d1f98ce4dc4f9055473b
IndiraPD <34646827+IndiraPD@users.noreply.github.com>
2018-06-27 15:42:04 +0530

    Create postitive_negative.java

---
+++ Assignments/Set1/postitive_negative.java
@@ -0,0 +1,27 @@
+package assignment;
+
+import java.util.Scanner;
+
+public class Odd {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+		if(n>0)
+		{
+			System.out.println("positive");
+		}
+		else if(n<0)
+		{
+			System.out.println("negative");
+		}
+		else
+		{
+			System.out.println("zero");
+		}
+		s.close();
+
+	}
+
+}
