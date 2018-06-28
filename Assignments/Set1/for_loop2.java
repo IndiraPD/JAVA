746718ec39ec2f7f92bd8128cf3405f96bbce025
IndiraPD <34646827+IndiraPD@users.noreply.github.com>
2018-06-28 11:54:09 +0530

    Create for_loop2.java

---
+++ Assignments/Set1/for_loop2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+
+
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+		for(int i=0;i<=n;i+=2){
+			System.out.println(i);
+		}
+		
+		s.close();
+	}
+}
